a8d15cfccc26487e1bc8c2f85dc0aecd46b70eaf
Azahar2 <a.zahar@baro-pfannenstein.de>
2021-05-24 12:46:38 +0200

    Added a shop page, collection preview componenent and collection item component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Route } from "react-router-dom";
 
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
       <Route exact path="/" component={HomePage} />
+      <Route exact path="/shop" component={ShopPage} />
     </div>
   );
 }
